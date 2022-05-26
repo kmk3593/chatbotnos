aa3982d1e420bb9ab0565a0d7356e199fcd1c26e
kmk3593 <alsrbs0219@gmail.com>
2022-05-26 17:16:09 +0900

    updated

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!122'
+    return 'Hello, World!3'
 
 # 카카오톡 텍스트형 응답
 @app.route('/api/sayHello', methods=['POST'])
