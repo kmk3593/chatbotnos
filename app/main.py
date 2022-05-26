8cb715af2bc1b4afabb7ab641f16a717d0e361fb
kmk3593 <alsrbs0219@gmail.com>
2022-05-26 16:47:55 +0900

    updated

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!11122222222'
+    return 'Hello, World!111223333333333'
 
 # 카카오톡 텍스트형 응답
 @app.route('/api/sayHello', methods=['POST'])
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 git add .
-git commit -m "updated"
+git commit -am "updated"
 git push origin main
 git push heroku main
 
